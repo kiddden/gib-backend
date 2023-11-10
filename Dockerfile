86fbf6cd165f149e75f1b70a7146226dae127a9c
kiddden <kazzer@knu.ua>
2023-11-10 16:19:00 +0200

    upd to host on google cloud

--- Dockerfile
+++ Dockerfile
@@ -72,6 +72,11 @@ USER vapor:vapor
 # Let Docker bind to port 8080
 EXPOSE 8080
 
+ENV DATABASE_HOST=localhost
+ENV DATABASE_PORT=5432
+ENV DATABASE_USERNAME=kazzer
+ENV DATABASE_NAME=myDB
+
 # Start the Vapor service when the image is run, default to listening on 8080 in production environment
 ENTRYPOINT ["./Run"]
-CMD ["serve", "--env", "production", "--hostname", "0.0.0.0", "--port", "8080"]
+CMD ["serve", "--env", "production", "--hostname", "0.0.0.0"]
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,6 +7,8 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
+    app.http.server.configuration.port = Int(Environment.get("PORT") ?? "8080")!
+    
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
